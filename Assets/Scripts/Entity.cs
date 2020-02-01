cf01b0db5bce09f088113c298af81e6f3fa771a6
Maengast <adntouch@gmail.com>
2020-02-01 03:53:12 +0100

    deplacement perso & rotation

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+
+	public float Speed;
+	public float RotationSpeed;
+	/**
+     * Init entity Instance
+     */
+    protected virtual void Init()
+    {
+        
+    }
+
+    /**
+     * Move entity to a specific target
+     */
+    protected virtual void Move(Transform target)
+    {
+	    
+    }
+    
+    /**
+     * Move entity along a vector
+     */
+    protected virtual void Move(Vector3 moveDirection)
+    {
+	    
+    }
+
+    protected void Rotate(Vector3 rotDirection)
+    {
+	    float angle = Mathf.Atan2(rotDirection.x, rotDirection.y) * Mathf.Rad2Deg;
+	    transform.rotation = Quaternion.AngleAxis(angle,Vector3.forward);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Entity
+{
+	private CharacterController CharCtrl;
+    // Start is called before the first frame update
+    void Start()
+    {
+	    CharCtrl = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frames
+    void Update()
+    {
+	    Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"),0);
+	    Vector3 rotDirection = new Vector3(Input.GetAxis("Rot_H"),Input.GetAxis("Rot_V"),0);
+	    
+	    Move(moveDirection);
+	    if (rotDirection.x != 0 || rotDirection.y != 0)
+	    {
+		    Rotate(rotDirection);
+	    }
+	    
+    }
+
+    protected override void Move(Vector3 moveDirection)
+    {
+	    moveDirection *= Speed;
+	    CharCtrl.Move( moveDirection * Time.deltaTime);
+    }
+}
